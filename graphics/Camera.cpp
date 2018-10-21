8fd4851d981f38ddb53d9074ed396c7caf5cd395
Bytenka <slybytenka@gmail.com>
2018-10-21 19:13:04 +0200

    Fixing camera behavior

--- graphics/Camera.cpp
+++ graphics/Camera.cpp
@@ -5,16 +5,17 @@
 
 namespace tk
 {
-Camera::Camera(glm::dvec3 initialPos, glm::dvec3 initialDir)
-	: m_position(initialPos), m_forward(glm::normalize(initialDir))
+Camera::Camera(glm::dvec3 position, glm::dvec3 target)
+	: m_position(position), m_direction(glm::normalize(position - target))
 {
 	// Gets the local right vector by using the direction vector and up vector,
 	// and getting the "normal" vector of the plane created by these vectors (done with cross product)
-	m_yaw = glm::asin(initialDir.y);
-	m_pitch = glm::pow(glm::atan(initialDir.x, initialDir.z), 2);
+	//m_yaw = glm::asin(initialDir.y);
+	//m_pitch = glm::pow(glm::atan(initialDir.x, initialDir.z), 2);
 
-	m_left = glm::normalize(glm::cross(glm::dvec3(0.0, 1.0, 0.0), m_forward));
-	m_up = glm::normalize(glm::cross(m_left, m_forward));
+	m_right = glm::normalize(glm::cross(glm::dvec3(0.0, 1.0, 0.0), m_direction));
+	m_up = glm::normalize(glm::cross(m_right, m_direction));
+	m_forward = glm::normalize(glm::cross(m_right, m_up));
 }
 
 Camera::~Camera()
@@ -28,7 +29,7 @@ void Camera::updateTransform()
 void Camera::rotate(double yaw, double pitch)
 {
 	m_yaw += yaw;
-	m_pitch += pitch;
+	m_pitch -= pitch;
 
 	if (m_pitch < -90.0)
 		m_pitch = -90.0;
@@ -42,20 +43,20 @@ void Camera::rotate(double yaw, double pitch)
 	if (m_yaw < -180.0)
 		m_yaw += 360.0;
 
-	glm::dvec3 newRot = glm::dvec3(0.0);
-	// Should not have a minus sign...
-	newRot.x = cos(glm::radians(-m_pitch)) * cos(glm::radians(m_yaw));
-	newRot.y = sin(glm::radians(-m_pitch));
-	newRot.z = cos(glm::radians(-m_pitch)) * sin(glm::radians(m_yaw));
+	glm::dvec3 newRot;
+	newRot.x = cos(glm::radians(m_pitch)) * cos(glm::radians(m_yaw));
+	newRot.y = sin(glm::radians(m_pitch));
+	newRot.z = cos(glm::radians(m_pitch)) * sin(glm::radians(m_yaw));
 
-	m_forward = glm::normalize(newRot);
-	m_left = glm::normalize(glm::cross(glm::dvec3(0.0, 1.0, 0.0), m_forward));
-	m_up = glm::normalize(glm::cross(m_forward, m_left)); // Should be m_left, m_forward
+	m_direction = glm::normalize(newRot);
+	m_right = glm::normalize(glm::cross(m_direction, glm::dvec3(0.0, 1.0, 0.0)));
+	m_up = glm::normalize(glm::cross(m_direction, m_right));
+	m_forward = glm::normalize(glm::cross(m_right, glm::dvec3(0.0, 1.0, 0.0)));
 }
 
 glm::mat4 Camera::getView() const
 {
-	return glm::lookAt(m_position, m_position + m_forward, m_up);
+	return glm::lookAt(m_position, m_position + m_direction, glm::dvec3(0.0, 1.0, 0.0));
 }
 
 void Camera::move(glm::dvec3 direction)
--- graphics/Camera.h
+++ graphics/Camera.h
@@ -14,17 +14,18 @@ class Camera
 {
   public:
 	Camera(
-		glm::dvec3 initialPos = glm::dvec3(0.0, 0.0, 0.0),
-		glm::dvec3 initialDir = glm::dvec3(0.0, 0.0, 1.0));
+		glm::dvec3 position = glm::dvec3(0.0, 0.0, 0.0),
+		glm::dvec3 target = glm::dvec3(0.0, 0.0, -1.0));
 	~Camera();
 
 	void move(glm::dvec3 direction);
 	glm::mat4 getView() const;
 	void rotate(double pitch, double yaw);
 
+	inline glm::dvec3 getDirection() const { return m_direction; }
+	inline glm::dvec3 getForward() const { return m_forward; }
 	inline glm::dvec3 getPosition() const { return m_position; }
-	inline glm::dvec3 getDirection() const { return m_forward; }
-	inline glm::dvec3 getLeft() const { return m_left; }
+	inline glm::dvec3 getRight() const { return m_right; }
 	inline glm::dvec3 getUp() const { return m_up; }
 
 	//glm::vec3 forward;
@@ -33,11 +34,12 @@ class Camera
 	void updateTransform();
 
 	glm::dvec3 m_position;
+	glm::dvec3 m_direction;
 
-	glm::dvec3 m_forward;
-	glm::dvec3 m_left;
+	glm::dvec3 m_forward; // Like direction, but it's the way, not where the player looks
+	glm::dvec3 m_right;
 	glm::dvec3 m_up;
 
-	double m_yaw, m_pitch;
+	double m_yaw = 0, m_pitch = 0;
 };
 } // namespace tk
